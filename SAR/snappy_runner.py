395dd7dfa0ce08a6d2a06c66f18c125dac6f69af
Jales Bussinguer <jalesbussinguer@gmail.com>
2022-02-14 09:50:15 -0300

    slc routines

---
+++ SAR/snappy_runner.py
@@ -0,0 +1,3 @@
+import subprocess
+
+pipeline_out = subprocess.call(['python', 'C:/Users/jales/Documents/GitHub/thesis_code/SAR/SLC_processing.py', '-j', 'C:/Users/jales/Documents/GitHub/thesis_code/slc_settings.json'])
