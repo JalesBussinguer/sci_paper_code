2030347022f9f573d818c50831d5e5cebda8b0da
Jales Bussinguer <jalesbussinguer@gmail.com>
2022-02-27 14:55:44 -0300

    27/02/2022

--- veg_indices/SAR_vegetation_indices.py
+++ veg_indices/SAR_vegetation_indices.py
@@ -6,7 +6,6 @@ from rasterio.mask import mask
 import geopandas as gpd
 
 import os
-
 import time
 from functools import wraps
 
@@ -26,7 +25,8 @@ def _get_args():
 
     return args
 
-def timing(func): # Processing time decorator
+# Processing time decorator
+def timing(func): 
     @wraps(func)
     def processing_time(*args, **kwargs):
         t1 = time.time()
@@ -35,81 +35,6 @@ def timing(func): # Processing time decorator
         print(f'@timing: {func.__name__} took {t2-t1} seconds')
         return result
     return processing_time
-
-# Optical indices
-@timing
-def ndvi_index(nir, red):
-
-    """
-    NDVI - Normalized Difference Vegetation Index
-
-    Args:
-        nir (array): near infrared band (S2-B8)
-        red (array): red band (S2-B4)
-
-    Returns:
-        NDVI (array)
-    """
-
-    ndvi = (nir - red) / (nir + red)
-    
-    return ndvi.astype(np.float32)
-
-@timing
-def ndwi_index(nir, swir1):
-
-    """
-    NDWI - Normalized Difference Water Index
-
-    Args:
-        nir (array): near infrared band (S2-B8)
-        swir1 (array) = short wave infrared band (S2-B11)
-
-    Returns:
-        NDWI (array)
-    """
-
-    ndwi = (nir - swir1) / (nir + swir1)
-    
-    return ndwi.astype(np.float32)
-
-@timing
-def psri_index(red, blue, redge2):
-
-    """
-    PSRI - Plant Senescence Reflectance Index
-
-    Args:
-    red (array) = red band (S2-B4)
-    blue (array) = blue band (S2-B2)
-    redge2 (array) = red edge 2 band (S2-B6)
-
-    Returns:
-        PSRI (array)
-    """
-
-    psri = (red - blue) / redge2
-    
-    return psri.astype(np.float32)
-
-@timing
-def evi_index(nir, red, blue):
-
-    '''
-    EVI - Enhanced Vegetation Index
-
-    Args:
-    nir (array) = near infrared band (S2-B8)
-    red (array) = red band (S2-B4)
-    blue (array) = blue band (S2-B2)
-    
-    Returns:
-        EVI (array) 
-    '''
-
-    evi = 2.5 * ((nir - red) / (nir + (6 * red) - (7.5 * blue) + 1))
-
-    return evi.astype(np.float32)
 
 # Tools
 def conv2d(matrix, window):
@@ -191,24 +116,6 @@ def dpsvim_index(vv, vh):
     dpsvim = (np.power(vv, 2) + (vv * vh)) / (1.4142)
 
     return dpsvim.astype(np.float32)
-
-@timing
-def dprvic_index(vh, vv):
-
-    q = vh/vv
-
-    dprvic = (q * (q + 3)) / np.power((q + 1), 2)
-
-    return dprvic.astype(np.float32)
-
-@ timing
-def rvic_index(vh, vv):
-
-    q = vh/vv
-
-    rvic = (4 * q) / (1 + q)
-
-    return rvic.astype(np.float32)
 
 # SLC indices    
 @timing
@@ -310,28 +217,6 @@ def prvi_index(c11, c12_real, c12_imag, c22, window_size):
 
     return prvi.astype(np.float32)
 
-# @timing
-# def rvi_slc_index(c11, c22, window_size):
-
-#     kernel = np.ones((window_size, window_size), np.float32) / (window_size * window_size)
-
-#     c11_T1 = c11
-#     c22_T1 = c22
-
-#     c11_T1r = conv2d(np.real(c11_T1), kernel)
-#     c11_T1i = conv2d(np.imag(c11_T1), kernel)
-#     c11s = c11_T1r + 1j * c11_T1i
-
-#     c22_T1r = conv2d(np.real(c22_T1), kernel)
-#     c22_T1i = conv2d(np.imag(c22_T1), kernel)
-#     c22s = c22_T1r + 1j * c22_T1i
-    
-#     c2_trace = c11s + c22s
-
-#     rvi = (4 * c22s) / c2_trace
-
-#     return rvi.astype(np.float32)
-
 @timing
 def _main(settings):
 
@@ -339,74 +224,63 @@ def _main(settings):
 
     geometries = [geom for geom in roi.geometry]
 
+    indices_list = []
+
     for item in os.listdir(settings['slc_image']):
 
-        if item.endswith('.zip'):
+        if item.endswith('.tif'):
 
-            date = item.split('_')[5]
+            date = item.split('_')[2]
 
+            file = settings['slc_image'] + '/' + item
 
-    # # Optical image
-    # with rst.open(settings['optical_image']) as opt:
-        
-    #     opt_image, opt_transform = mask(opt, geometries, crop=True, nodata=np.nan)
-        
-    #     swir1 = opt_image[0]
-    #     nir = opt_image[1]
-    #     redge2 = opt_image[2]
-    #     red = opt_image[3]
-    #     blue = opt_image[4]
+            # SAR SLC image
+            with rst.open(file) as slc:
 
-    # SAR GRD image
-    with rst.open(settings['grd_image']) as grd:
+                slc_image, slc_transform = mask(slc, geometries, crop=True, nodata=np.nan)
 
-        grd_image, grd_transform = mask(grd, geometries, crop=True, nodata=np.nan)
-
-        vh = grd_image[0]
-        vv = grd_image[1]
+                c11 = slc_image[0]
+                c12_real = slc_image[1]
+                c12_imag = slc_image[2]
+                c22 = slc_image[3]
     
-    # SAR SLC image
-    with rst.open(settings['slc_image']) as slc:
+            # DpRVI
+            dprvi = dprvi_index(c11, c12_real, c12_imag, c22, window_size=1)
+            indices_list.append(dprvi)
 
-        slc_image, slc_transform = mask(slc, geometries, crop=True, nodata=np.nan)
+            # PRVI
+            prvi = prvi_index(c11, c12_real, c12_imag, c22, window_size=1)
+            indices_list.append(prvi)
 
-        c11 = slc_image[0]
-        c12_real = slc_image[1]
-        c12_imag = slc_image[2]
-        c22 = slc_image[3]
+    for item in os.listdir(settings['grd_image']):
+
+        if item.endswith('.tif'):
+
+            date = item.split('_')[2]
+
+            file = settings['grd_image'] + '/' + item
+
+            # SAR GRD image
+            with rst.open(file) as grd:
+
+                grd_image, grd_transform = mask(grd, geometries, crop=True, nodata=np.nan)
+
+                vh = grd_image[0]
+                vv = grd_image[1]
+            
+            # DPSVI
+            dpsvi = dpsvi_index(vv, vh)
+            indices_list.append(dpsvi)
+
+            # DPSVIm
+            dpsvim = dpsvim_index(vv, vh)
+            indices_list.append(dpsvim)
+
+            # RVI_GRD
+            rvi_grd = rvi_grd_index(vv, vh)
+            indices_list.append(rvi_grd)
     
-    indices_list = []
-    
-    # Multispectral vegetation indices
-    # NDVI
-    # ndvi = ndvi_index(nir, red)
-    # indices_list.append(ndvi)
-    # # NDWI
-    # ndwi = ndwi_index(nir, swir1)
-    # indices_list.append(ndwi)
-    # # PSRI
-    # psri = psri_index(red, blue, redge2)
-    # indices_list.append(psri)
-    # # EVI
-    # evi = evi_index(nir, red, blue)
-    # indices_list.append(evi)
-    
-    # SAR vegetation indices
-    # DpRVI
-    dprvi = dprvi_index(c11, c12_real, c12_imag, c22, window_size=1)
-    indices_list.append(dprvi)
-    # PRVI
-    prvi = prvi_index(c11, c12_real, c12_imag, c22, window_size=1)
-    indices_list.append(prvi)
-    # DPSVI
-    dpsvi = dpsvi_index(vv, vh)
-    indices_list.append(dpsvi)
-    # DPSVIm
-    dpsvim = dpsvim_index(vv, vh)
-    indices_list.append(dpsvim)
-    # RVI_GRD
-    rvi_grd = rvi_grd_index(vv, vh)
-    indices_list.append(rvi_grd)
+    func_list = ['DpRVI', 'PRVI', 'DPSVI', 'DPSVIm', 'RVI']
     
     out_meta = slc.meta
 
@@ -418,9 +292,11 @@ def _main(settings):
                     "count": len(indices_list)
                     })
 
-    with rst.open(settings['indices_outpath'], "w", **out_meta) as dest:
+    with rst.open(settings['indices_outpath'] + '/' + date + '_' + '32723', "w", **out_meta) as dest:
         for id, indice in enumerate(indices_list, start=1):
             dest.write(indice, id)
+            for func in func_list:
+                dest.set_band_description(id, func)
 
 if __name__ == "__main__":
 
