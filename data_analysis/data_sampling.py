1d6d9c713d4bf3cd326d007306e7512a1a35b8d8
Jales Bussinguer <jalesbussinguer@gmail.com>
2022-03-08 22:10:17 -0300

    Update data_sampling.py

--- data_analysis/data_sampling.py
+++ data_analysis/data_sampling.py
@@ -1,73 +1,117 @@
 import numpy as np
 import geopandas as gpd
+import rasterio as rst
+from rasterio.mask import mask
 import pandas as pd
 import os
 
-from rasterstats import point_query
-
-florestal_df_stats = pd.DataFrame()
-savanica_df_stats = pd.DataFrame()
-campestre_df_stats = pd.DataFrame()
+# from rasterstats import point_query
 
 form_florestal = gpd.read_file('D:/thesis_data/ROI/classes/form_florestal_10m_32723.GEOJSON')
+florestal_geom = [geom for geom in form_florestal.geometry]
+
 form_savanica = gpd.read_file('D:/thesis_data/ROI/classes/form_savanica_10m_32723.GEOJSON')
+savanica_geom = [geom for geom in form_savanica.geometry]
+
 form_campestre = gpd.read_file('D:/thesis_data/ROI/classes/form_campestre_10m_32723.GEOJSON')
+campestre_geom = [geom for geom in form_campestre.geometry]
 
-indices = os.listdir('D:/thesis_data/VEG_INDICES')
+indices_path = os.listdir('D:/thesis_data/VEG_INDICES/raster')
 
-for index, image in enumerate(indices):
+for index, image in enumerate(indices_path):
 
-    date = indices[index].split('.')[0]
+    date = indices_path[index].split('T')[0]
 
-    florestal = point_query(form_florestal, 'D:/thesis_data/VEG_INDICES/' + str(image), band=1, nodata=np.nan)
+    with rst.open('D:/thesis_data/VEG_INDICES/raster/' + str(image)) as raster:
+        indices_clipped, indices_transform = mask(raster, florestal_geom, crop=True, nodata=np.nan)
 
-    df_florestal = pd.DataFrame({'DpRVI': florestal})
+        dprvi = indices_clipped[0]
+        prvi = indices_clipped[1]
+        dpsvi = indices_clipped[2]
+        dpsvim = indices_clipped[3]
+        rvi = indices_clipped[4]
+
+    df_florestal = pd.DataFrame({'DpRVI': dprvi.flatten(), 'PRVI': prvi.flatten(), 'DPSVI': dpsvi.flatten(), 'DPSVIm': dpsvim.flatten(), 'RVI': rvi.flatten()})
     df_florestal.dropna(inplace=True)
-    df_florestal.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'florestal' + date + 'distribution' + '.csv', sep=',')
 
-    florestal_median = np.median(df_florestal['DpRVI'])
-    florestal_mean = np.mean(df_florestal['DpRVI'])
-    florestal_std = np.std(df_florestal['DpRVI'])
-
-    florestal_stats = pd.DataFrame({'date': date, 'median': florestal_median, 'mean': florestal_mean, 'std': florestal_std}, index=[0])
-    florestal_df_stats.append(florestal_stats)
+    df_florestal.to_csv('D:/thesis_data/VEG_INDICES/samples/' + 'florestal_' + date + '_distribution' + '.csv', sep=',')
 
     print(f'Florestal data of {date} collected!')
 
+    with rst.open('D:/thesis_data/VEG_INDICES/raster/' + str(image)) as raster:
+        indices_clipped, indices_transform = mask(raster, savanica_geom, crop=True, nodata=np.nan)
 
-    savanica = point_query(form_savanica, 'D:/thesis_data/VEG_INDICES/' + str(image), band=1, nodata=np.nan)
+        dprvi = indices_clipped[0]
+        prvi = indices_clipped[1]
+        dpsvi = indices_clipped[2]
+        dpsvim = indices_clipped[3]
+        rvi = indices_clipped[4]
 
-    df_savanica = pd.DataFrame({'DpRVI': savanica})
+    df_savanica = pd.DataFrame({'DpRVI': dprvi.flatten(), 'PRVI': prvi.flatten(), 'DPSVI': dpsvi.flatten(), 'DPSVIm': dpsvim.flatten(), 'RVI': rvi.flatten()})
     df_savanica.dropna(inplace=True)
-    df_savanica.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'savanica' + date + 'distribution' + '.csv', sep=',')
 
-    savanica_median = np.median(df_savanica['DpRVI'])
-    savanica_mean = np.mean(df_savanica['DpRVI'])
-    savanica_std = np.std(df_savanica['DpRVI'])
-
-    savanica_stats = pd.DataFrame({'date': date, 'median': savanica_median, 'mean': savanica_mean, 'std': savanica_std}, index=[0])
-    savanica_df_stats.append(savanica_stats)
+    df_savanica.to_csv('D:/thesis_data/VEG_INDICES/samples/' + 'savanica_' + date + '_distribution' + '.csv', sep=',')
 
     print(f'Savanica data of {date} collected!')
 
+    with rst.open('D:/thesis_data/VEG_INDICES/raster/' + str(image)) as raster:
+        indices_clipped, indices_transform = mask(raster, campestre_geom, crop=True, nodata=np.nan)
 
-    campestre = point_query(form_campestre, 'D:/thesis_data/VEG_INDICES/' + str(image), band=1, nodata=np.nan)
+        dprvi = indices_clipped[0]
+        prvi = indices_clipped[1]
+        dpsvi = indices_clipped[2]
+        dpsvim = indices_clipped[3]
+        rvi = indices_clipped[4]
 
-    df_campestre = pd.DataFrame({'DpRVI': campestre})
+    df_campestre = pd.DataFrame({'DpRVI': dprvi.flatten(), 'PRVI': prvi.flatten(), 'DPSVI': dpsvi.flatten(), 'DPSVIm': dpsvim.flatten(), 'RVI': rvi.flatten()})
     df_campestre.dropna(inplace=True)
-    df_campestre.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'campestre' + date + 'distribution' + '.csv', sep=',')
 
-    campestre_median = np.median(df_campestre['DpRVI'])
-    campestre_mean = np.mean(df_campestre['DpRVI'])
-    campestre_std = np.std(df_campestre['DpRVI'])
-
-    campestre_stats = pd.DataFrame({'date': date, 'median': campestre_median, 'mean': campestre_mean, 'std': campestre_std}, index=[0])
-    campestre_df_stats.append(campestre_stats)
+    df_campestre.to_csv('D:/thesis_data/VEG_INDICES/samples/' + 'campestre_' + date + '_distribution' + '.csv', sep=',')
 
     print(f'Campestre data of {date} collected!')
 
-florestal_df_stats.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'florestal' + date + 'stats' + '.csv', sep=',')
-savanica_df_stats.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'savanica' + date + 'stats' + '.csv', sep=',')
-campestre_df_stats.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'campestre' + date + 'stats' + '.csv', sep=',')
 
-print(f'DpRVI stats collected!')
+
+
+
+
+
+
+
+
+#     savanica = point_query(form_savanica, 'D:/thesis_data/VEG_INDICES/' + str(image), band=1, nodata=np.nan)
+
+#     df_savanica = pd.DataFrame({'DpRVI': savanica})
+#     df_savanica.dropna(inplace=True)
+#     df_savanica.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'savanica' + date + 'distribution' + '.csv', sep=',')
+
+#     savanica_median = np.median(df_savanica['DpRVI'])
+#     savanica_mean = np.mean(df_savanica['DpRVI'])
+#     savanica_std = np.std(df_savanica['DpRVI'])
+
+#     savanica_stats = pd.DataFrame({'date': date, 'median': savanica_median, 'mean': savanica_mean, 'std': savanica_std}, index=[0])
+#     savanica_df_stats.append(savanica_stats)
+
+#     print(f'Savanica data of {date} collected!')
+
+
+#     campestre = point_query(form_campestre, 'D:/thesis_data/VEG_INDICES/' + str(image), band=1, nodata=np.nan)
+
+#     df_campestre = pd.DataFrame({'DpRVI': campestre})
+#     df_campestre.dropna(inplace=True)
+#     df_campestre.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'campestre' + date + 'distribution' + '.csv', sep=',')
+
+#     campestre_median = np.median(df_campestre['DpRVI'])
+#     campestre_mean = np.mean(df_campestre['DpRVI'])
+#     campestre_std = np.std(df_campestre['DpRVI'])
+
+#     campestre_stats = pd.DataFrame({'date': date, 'median': campestre_median, 'mean': campestre_mean, 'std': campestre_std}, index=[0])
+#     campestre_df_stats.append(campestre_stats)
+
+#     print(f'Campestre data of {date} collected!')
+
+# florestal_df_stats.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'florestal' + date + 'stats' + '.csv', sep=',')
+# savanica_df_stats.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'savanica' + date + 'stats' + '.csv', sep=',')
+# campestre_df_stats.to_csv('D:/thesis_data/VEG_INDICES/samples/DpRVI/' + 'campestre' + date + 'stats' + '.csv', sep=',')
+
+# print(f'DpRVI stats collected!')
