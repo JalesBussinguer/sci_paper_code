bacec0eb912759650d593ec3fdcdc9c8a025174b
Jales Bussinguer <jalesbussinguer@gmail.com>
2022-04-07 12:15:17 -0300

    updates

---
+++ data_analysis/dem_sampling.py
@@ -0,0 +1,21 @@
+import numpy as np
+import geopandas as gpd
+import rasterio as rst
+from rasterio.mask import mask
+import pandas as pd
+import os
+
+pnb = gpd.read_file('D:/thesis_data/ROI/PNB_32723.GEOJSON')
+pnb_geom = [geom for geom in pnb.geometry]
+
+raster_path = 'D:/thesis_data/TOPOGRAPHY/slope_32723.tif'
+
+with rst.open(raster_path) as raster:
+    raster_clipped, raster_transform = mask(raster, pnb_geom, crop=True, nodata=-1)
+
+    dem = raster_clipped[0]
+
+df_dem = pd.DataFrame({'dem': dem.flatten()})
+df_dem.dropna(inplace=True)
+
+df_dem.to_csv('D:/thesis_data/TOPOGRAPHY/slope_pnb_samples.csv', sep=',', index=False)
