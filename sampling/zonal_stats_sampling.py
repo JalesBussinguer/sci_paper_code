7ebcd51d482c4d680065dc82855c10ce96f8effc
Jales Bussinguer <jalesbussinguer@gmail.com>
2022-04-16 17:32:26 -0300

    updates - 16/04/2021

--- sampling/zonal_stats_sampling.py
+++ sampling/zonal_stats_sampling.py
@@ -23,7 +23,7 @@ for id, indice in enumerate(indices_list, start=1):
 
         date = indices_path[index].split('T')[0]
 
-        florestal = zonal_stats(form_florestal, 'D:/thesis_data/VEG_INDICES/raster/' + image, band=id, nodata=np.nan, stats=['median', 'percentile_25', 'percentile_75', 'std'])
+        florestal = zonal_stats(form_florestal, 'D:/thesis_data/VEG_INDICES/raster/' + image, band=id, nodata=np.nan, stats=['mean', 'median', 'percentile_25', 'percentile_75', 'std'])
 
         florestal_stats = pd.DataFrame(florestal)
         florestal_stats['date'] = pd.to_datetime(int(date), format='%Y%m%d')
@@ -31,7 +31,7 @@ for id, indice in enumerate(indices_list, start=1):
 
         print(f'{indice} - {date} florestal data collected')
 
-        savanica = zonal_stats(form_savanica, 'D:/thesis_data/VEG_INDICES/raster/' + image, band=id, nodata=np.nan, stats=['median', 'percentile_25', 'percentile_75', 'std'])
+        savanica = zonal_stats(form_savanica, 'D:/thesis_data/VEG_INDICES/raster/' + image, band=id, nodata=np.nan, stats=['mean','median', 'percentile_25', 'percentile_75', 'std'])
 
         savanica_stats = pd.DataFrame(savanica)
         savanica_stats['date'] = pd.to_datetime(int(date), format='%Y%m%d')
@@ -39,7 +39,7 @@ for id, indice in enumerate(indices_list, start=1):
 
         print(f'{indice} - {date} savanica data collected!')
 
-        campestre = zonal_stats(form_campestre, 'D:/thesis_data/VEG_INDICES/raster/' + image, band=id, nodata=np.nan, stats=['median', 'percentile_25', 'percentile_75', 'std'])
+        campestre = zonal_stats(form_campestre, 'D:/thesis_data/VEG_INDICES/raster/' + image, band=id, nodata=np.nan, stats=['mean','median', 'percentile_25', 'percentile_75', 'std'])
 
         campestre_stats = pd.DataFrame(campestre)
         campestre_stats['date'] = pd.to_datetime(int(date), format='%Y%m%d')
